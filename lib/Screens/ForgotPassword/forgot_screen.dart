6b0f3bcec848f3c35f87cf19c0e6f2dd7aad516a
sundipthapa <lakhanmgr123@gmail.com>
2022-12-09 13:19:52 +0545

    :lipstick: validation

--- lib/Screens/ForgotPassword/forgot_screen.dart
+++ lib/Screens/ForgotPassword/forgot_screen.dart
@@ -9,6 +9,10 @@ class ForgotScreen extends StatefulWidget {
 
 class _ForgotScreenState extends State<ForgotScreen> {
   var _isVisible = false;
+
+  TextEditingController email = TextEditingController();
+  TextEditingController passoword = TextEditingController();
+  final form = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
     final deviceHeight = MediaQuery.of(context).size.height;
@@ -20,68 +24,87 @@ class _ForgotScreenState extends State<ForgotScreen> {
       ),
       body: SafeArea(
         child: SingleChildScrollView(
-          child: Column(
-            children: [
-              Container(
-                padding: EdgeInsets.only(top: 110),
-                // color: Colors.blue,
-                height: deviceHeight * 0.40,
-                child: Image.asset('assets/images/HamroPasal.png'),
-              ),
-              Container(
-                height: deviceHeight * 0.60,
-                width: double.infinity,
-                margin: EdgeInsets.symmetric(horizontal: 20),
-                child: LayoutBuilder(builder: (ctx, constraints) {
-                  return SafeArea(
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: [
-                        const Text(
-                          'Reset your password',
-                          style: TextStyle(
-                            fontSize: 30,
-                            fontWeight: FontWeight.bold,
+          child: Form(
+            key: form,
+            child: Column(
+              children: [
+                Container(
+                  padding: EdgeInsets.only(top: 110),
+                  // color: Colors.blue,
+                  height: deviceHeight * 0.50,
+                  child: Image.asset(
+                    'assets/images/HamroPasal.png',
+                    height: 400,
+                  ),
+                ),
+                Container(
+                  height: deviceHeight * 0.60,
+                  width: double.infinity,
+                  margin: EdgeInsets.symmetric(horizontal: 20),
+                  child: LayoutBuilder(builder: (ctx, constraints) {
+                    return SafeArea(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          const Text(
+                            'Reset your password',
+                            style: TextStyle(
+                              fontSize: 25,
+                              fontWeight: FontWeight.bold,
+                            ),
                           ),
-                        ),
-                        SizedBox(height: constraints.maxHeight * 0.04),
-                        Container(
-                          height: constraints.maxHeight * 0.12,
-                          decoration: BoxDecoration(
-                              color: Color(0xffB4B4B4).withOpacity(0.4),
-                              borderRadius: BorderRadius.circular(30)),
-                          child: const Padding(
-                            padding: EdgeInsets.only(left: 15),
+                          SizedBox(height: constraints.maxHeight * 0.04),
+                          Container(
                             child: Center(
-                              child: TextField(
+                              child: TextFormField(
+                                controller: email,
+                                validator: (String? value) {
+                                  if (value!.isEmpty ||
+                                      !RegExp(r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+")
+                                          .hasMatch(value)) {
+                                    return 'Please enter valid email';
+                                  }
+                                  return null;
+                                },
                                 decoration: InputDecoration(
+                                  filled: true,
+                                  fillColor: Colors.grey[350],
+                                  border: OutlineInputBorder(
+                                    borderRadius: BorderRadius.circular(30),
+                                    borderSide: BorderSide.none,
+                                  ),
                                   prefixIcon: Icon(
                                     Icons.email,
-                                    color: Colors.deepOrange,
+                                    color: Color(0xFFF57C00),
                                     size: 25,
                                   ),
-                                  border: InputBorder.none,
-                                  hintText: 'Enter your valid email',
+                                  hintText: 'Enter your email',
                                 ),
                               ),
                             ),
                           ),
-                        ),
-                        SizedBox(height: constraints.maxHeight * 0.04),
-                        Container(
-                          height: constraints.maxHeight * 0.12,
-                          decoration: BoxDecoration(
-                              color: Color(0xffB4B4B4).withOpacity(0.4),
-                              borderRadius: BorderRadius.circular(30)),
-                          child: Padding(
-                            padding: const EdgeInsets.only(left: 15),
+                          SizedBox(height: constraints.maxHeight * 0.04),
+                          Container(
                             child: Center(
-                              child: TextField(
+                              child: TextFormField(
+                                controller: passoword,
+                                validator: (String? value) {
+                                  if (value == null || value.isEmpty) {
+                                    return 'Password are required';
+                                  }
+                                  return null;
+                                },
                                 obscureText: _isVisible ? false : true,
                                 decoration: InputDecoration(
+                                    filled: true,
+                                    fillColor: Colors.grey[350],
+                                    border: OutlineInputBorder(
+                                      borderRadius: BorderRadius.circular(30),
+                                      borderSide: BorderSide.none,
+                                    ),
                                     prefixIcon: const Icon(
                                       Icons.lock,
-                                      color: Colors.deepOrange,
+                                      color: Color(0xFFF57C00),
                                       size: 25,
                                     ),
                                     suffixIcon: IconButton(
@@ -97,41 +120,50 @@ class _ForgotScreenState extends State<ForgotScreen> {
                                         color: Colors.grey,
                                       ),
                                     ),
-                                    border: InputBorder.none,
                                     hintText: "Enter new password"),
                               ),
                             ),
                           ),
-                        ),
-                        Container(
-                          width: double.infinity,
-                          height: constraints.maxHeight * 0.12,
-                          margin: EdgeInsets.only(
-                            top: constraints.maxHeight * 0.05,
-                          ),
-                          child: ElevatedButton(
-                            onPressed: () {},
-                            style: ElevatedButton.styleFrom(
-                              primary: Colors.deepOrange,
-                              shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(30),
+                          Container(
+                            width: 300,
+                            height: constraints.maxHeight * 0.14,
+                            margin: EdgeInsets.only(
+                              top: constraints.maxHeight * 0.05,
+                            ),
+                            child: ElevatedButton(
+                              onPressed: () {
+                                if (form.currentState!.validate()) {
+                                  ScaffoldMessenger.of(context).showSnackBar(
+                                    SnackBar(
+                                      content: Text("Reset successfully"),
+                                    ),
+                                  );
+                                } else {
+                                  print("Invalid Form");
+                                }
+                              },
+                              style: ElevatedButton.styleFrom(
+                                primary: Color(0xFFF57C00),
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(30),
+                                ),
+                              ),
+                              child: const Text(
+                                'Reset',
+                                style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 22,
+                                ),
                               ),
                             ),
-                            child: const Text(
-                              'Reset',
-                              style: TextStyle(
-                                fontWeight: FontWeight.bold,
-                                fontSize: 22,
-                              ),
-                            ),
                           ),
-                        ),
-                      ],
-                    ),
-                  );
-                }),
-              )
-            ],
+                        ],
+                      ),
+                    );
+                  }),
+                )
+              ],
+            ),
           ),
         ),
       ),
