649fa4b9b9235699782005bdbbe70ed4e42d1e10
admiral41 <kafle.susan671@outlook.com>
2022-12-15 18:05:00 +0545

    :lipstick: added design to dashboard

--- lib/Screens/homeScreen/homeScreen.dart
+++ lib/Screens/homeScreen/homeScreen.dart
@@ -9,6 +9,32 @@ class HomeScreen extends StatelessWidget {
     return Scaffold(
       body: ListView(children: [
         HomeAppBar(),
+        Container(
+          //temporrary height
+          height: 800,
+          padding: EdgeInsets.only(top: 15),
+          decoration: BoxDecoration(
+            color: Color(0xFFF57C00),
+            borderRadius: BorderRadius.only(
+              topRight: Radius.circular(35),
+              topLeft: Radius.circular(35),
+            ),
+          ),
+          child: Column(
+            children: [
+              Container(
+                margin: EdgeInsets.symmetric(horizontal: 10),
+                padding: EdgeInsets.symmetric(horizontal: 10),
+                height: 50,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(30),
+                ),
+              ),
+            ],
+          ),
+
+        )
 
       ],
       ),
