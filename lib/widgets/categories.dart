4a3e5671400a9b1d71fca22c39869085e1cd8866
admiral41 <kafle.susan671@outlook.com>
2022-12-15 18:46:24 +0545

    :lipstick: made categories selectable

--- lib/widgets/categories.dart
+++ lib/widgets/categories.dart
@@ -32,6 +32,17 @@ class _CategorySelectorState extends State<CategorySelector> {
                   selectedIndex=index;
                 });
               },
+              child: Padding(
+                padding: EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+                child: Text(
+                  categories[index],
+                  style: TextStyle(
+                      color: index==selectedIndex ? Colors.white : Colors.white60,
+                      fontSize: 24.0,
+                      fontWeight: FontWeight.bold,
+                      letterSpacing: 1.2),
+                ),
+              ),
             );
           },
         )
