a81ac3d61e658a9b1cf52bd0ced973a6ea41daa5
sundipthapa <lakhanmgr123@gmail.com>
2022-12-18 12:28:00 +0545

    :lipstick: hoodies section added

--- lib/Screens/homeScreen/homepage_data.dart
+++ lib/Screens/homeScreen/homepage_data.dart
@@ -107,3 +107,110 @@ List<SingleProductModel> sigleProductData = [
           'https://assets.reebok.com/images/w_600,f_auto,q_auto/53b4eac625434cffbfc1ab1400e1f2bc_9366/Classics_Archive_Backpack_White_FL7044_04_standard.jpg'),
 ];
 ////////////////////////////// Hoodies.............
+List<SingleProductModel> colothsData = [
+  SingleProductModel(
+      productImage:
+          'https://res.cloudinary.com/dyfwfkx4t/image/upload/v1671104614/Hd865df175eca4203bfa3055a3f6cf7ab8.jpg_720x720q50_hgacry.jpg',
+      productModel: 'Short',
+      productName: 'Textured epic shorts',
+      productOldPrice: 50,
+      productPrice: 44,
+      productFourImage:
+          'https://assets.reebok.com/images/w_600,f_auto,q_auto/cbfbdc0f93f04f7f8f79abea0149c1f5_9366/Textured_Epic_Shorts_Green_FU2863.jpg',
+      productSecondImage:
+          'https://assets.reebok.com/images/w_600,f_auto,q_auto/2eec510c76a342098e06abea01447b90_9366/Textured_Epic_Shorts_Green_FU2863_01_standard.jpg',
+      productThirdImage:
+          'https://assets.reebok.com/images/w_600,f_auto,q_auto/f0e92e197adf486f8c32abea01492aae_9366/Textured_Epic_Shorts_Green_FU2863_03_standard_hover.jpg'),
+  SingleProductModel(
+      productImage:
+          'https://static-01.daraz.com.np/p/d8182e5102fa6a70dd3007451f5edd6a.jpg',
+      productModel: 'jacket',
+      productName: 'Classics vector track jacket',
+      productOldPrice: 100,
+      productPrice: 90,
+      productFourImage:
+          'https://assets.reebok.com/images/w_600,f_auto,q_auto/8b44edfc2f88468482d6aae7017aa580_9366/Classics_Twin_Vector_Track_Jacket_Blue_FJ3172_01_standard.jpg',
+      productSecondImage:
+          'https://assets.reebok.com/images/w_600,f_auto,q_auto/c22808333d844df787d1aae7017a57ac_9366/Classics_Twin_Vector_Track_Jacket_Blue_FJ3172_03_standard_hover.jpg',
+      productThirdImage:
+          'https://assets.reebok.com/images/w_600,f_auto,q_auto/38f6634ec24e4367a66faae7017c26c0_9366/Classics_Twin_Vector_Track_Jacket_Blue_FJ3172_07_detail.jpg'),
+  SingleProductModel(
+      productImage:
+          'https://res.cloudinary.com/dyfwfkx4t/image/upload/c_scale,h_600,w_600/v1671103395/H5bc0a5aa0eb74ad286443ac69a64f27em.jpg_720x720q50_nmzomm.jpg',
+      productModel: 'Jacket',
+      productName: 'Utility field vest',
+      productOldPrice: 80,
+      productPrice: 65,
+      productFourImage:
+          'https://assets.reebok.com/images/w_600,f_auto,q_auto/f73d70d3e8c94d32bfd1ac8a00fb6f2f_9366/Utility_Field_Vest_Black_GL3129_07_detail.jpg',
+      productSecondImage:
+          'https://assets.reebok.com/images/w_600,f_auto,q_auto/277cecec6e534308be1eac8a00fb67fd_9366/Utility_Field_Vest_Black_GL3129_06_detail.jpg',
+      productThirdImage:
+          'https://assets.reebok.com/images/w_600,f_auto,q_auto/83c8780bd2534e4c9a76ac8a00fb754c_9366/Utility_Field_Vest_Black_GL3129_13_standard.jpg'),
+  SingleProductModel(
+      productImage:
+          'https://res.cloudinary.com/dyfwfkx4t/image/upload/v1671272025/Ha8654a8bad74475e953687e9bc72be02V.jpg_300x300_jhzfkm.jpg',
+      productModel: 'Sweat-Shirts',
+      productName: 'Myt crew sweatshirt',
+      productOldPrice: 50,
+      productPrice: 40,
+      productFourImage:
+          'https://assets.reebok.com/images/w_600,f_auto,q_auto/6294704fdcd3493db788ac8a011f7433_9366/MYT_Crew_Sweatshirt_Green_GL3241_01_standard.jpg',
+      productSecondImage:
+          'https://assets.reebok.com/images/w_600,f_auto,q_auto/065d4a51189d46eaa5f6ac8400ff9e39_9366/Jersey_de_cuello_redondo_MYT_Verde_GL3241_13_standard.jpg',
+      productThirdImage:
+          'https://assets.reebok.com/images/w_600,f_auto,q_auto/97e2a8cffc3d4803bc6bac8a01202834_9366/MYT_Crew_Sweatshirt_Green_GL3241_07_detail.jpg'),
+  SingleProductModel(
+      productImage:
+          'https://res.cloudinary.com/dyfwfkx4t/image/upload/v1671272235/A9f02a6fb3d284324ba6d55e6f6250424w.png_300x300_ctaxel.png',
+      productModel: 'T-shirt',
+      productName: 'Allen Iverson I3 3I Logo T-Shirt',
+      productOldPrice: 40,
+      productPrice: 30,
+      productFourImage:
+          'https://assets.reebok.com/images/w_600,f_auto,q_auto/3705e28fc76c462abbecacf10033b39d_9366/Allen_Iverson_I3_3I_Logo_T-Shirt_Multi_EY0885_03_standard_hover.jpg',
+      productSecondImage:
+          'https://assets.reebok.com/images/w_600,f_auto,q_auto/4a84ea20a9ec4b3b8864acf1002f6623_9366/Allen_Iverson_I3_3I_Logo_T-Shirt_Multi_EY0885_05_detail.jpg',
+      productThirdImage:
+          'https://assets.reebok.com/images/w_600,f_auto,q_auto/27ea5cd9696f45b8968aacf1002bc8ce_9366/Allen_Iverson_I3_Dimensional_Logo_T-Shirt_Multi_EY0880_01_standard.jpg'),
+  SingleProductModel(
+      productImage:
+          'https://res.cloudinary.com/dyfwfkx4t/image/upload/c_scale,w_600/v1671104157/Hba9b5155aee2443d970f0af59fcc642dt.png_720x720q50_ugccnx.png',
+      productModel: 'Pants',
+      productName: 'knit track pants',
+      productOldPrice: 70,
+      productPrice: 55,
+      productFourImage:
+          'https://assets.reebok.com/images/w_600,f_auto,q_auto/e9a41b48d1744c628cb3ac6800a68675_9366/Knit_Track_Pants_Black_GL3098_01_standard.jpg',
+      productSecondImage:
+          'https://assets.reebok.com/images/w_600,f_auto,q_auto/7810f8fd93bf48fbbde2ac6800a68d81_9366/Knit_Track_Pants_Black_GL3098_03_standard_hover.jpg',
+      productThirdImage:
+          'https://media.handball-store.com/catalog/product/cache/image/573x/9df78eab33525d08d6e5fb8d27136e95/g/l/gl3098_app_virtual_standard_white.jpg'),
+  SingleProductModel(
+      productImage:
+          'https://res.cloudinary.com/dyfwfkx4t/image/upload/v1671104266/H62aa14061ef84811b4b7672a7c698dfey.jpg_720x720q50_uw2g0u.webp',
+      productModel: 'Tank-tops',
+      productName: 'Workout_Ready_Tech_Tee_Orange',
+      productOldPrice: 30,
+      productPrice: 20,
+      productFourImage:
+          'https://assets.reebok.com/images/w_600,f_auto,q_auto/c0fad2f48f42411b83a1abc701513d73_9366/Workout_Ready_Tech_Tee_Orange_FU3246_03_standard_hover.jpg',
+      productSecondImage:
+          'https://assets.reebok.com/images/w_600,f_auto,q_auto/10c0394a753b4173937dab3d00f85b58_9366/Workout_Ready_Tech_Tee_Orange_FU3246_13_standard.jpg',
+      productThirdImage:
+          'https://assets.reebok.com/images/w_600,f_auto,q_auto/e49f6db542a348e1a4d9abc701563f30_9366/Workout_Ready_Tech_Tee_Orange_FU3246_06_detail.jpg'),
+  SingleProductModel(
+      productImage:
+          'https://res.cloudinary.com/dyfwfkx4t/image/upload/v1671104401/H700c7634067a44bbbc2b290666dc7d36z.jpg_720x720q50_m0za4z.jpg',
+      productModel: 'Tank-tops',
+      productName: 'Classics mesh tank top',
+      productOldPrice: 40,
+      productPrice: 34,
+      productFourImage:
+          'https://assets.reebok.com/images/w_600,f_auto,q_auto/c38c40a799344c459d8aaabf0091464b_9366/Classics_Mesh_Tank_Top_White_FJ3179_13_standard.jpg',
+      productSecondImage:
+          'https://assets.reebok.com/images/w_600,f_auto,q_auto/d73cb08a533443118beaaae00139cf20_9366/Classics_Mesh_Tank_Top_White_FJ3179_03_standard_hover.jpg',
+      productThirdImage:
+          'https://assets.reebok.com/images/w_600,f_auto,q_auto/af51c8a6349847c8ba7eaae001360973_9366/Classics_Mesh_Tank_Top_White_FJ3179.jpg'),
+];
+////////////////////////////// shoes.............
