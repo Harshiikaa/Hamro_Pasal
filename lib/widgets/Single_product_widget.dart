70068e6c57e61a03ee858bcfe5372d1f825f54a7
sundipthapa <lakhanmgr123@gmail.com>
2022-12-18 12:22:19 +0545

    :lipstick: file modified on widgets folder

--- lib/widgets/Single_product_widget.dart
+++ lib/widgets/Single_product_widget.dart
@@ -35,6 +35,101 @@ class _SingleProductWidgetState extends State<SingleProductWidget> {
           color: Colors.yellow,
           borderRadius: BorderRadius.circular(16),
         ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Expanded(
+              child: Container(
+                alignment: Alignment.topRight,
+                width: double.infinity,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(16),
+                  color: Colors.transparent,
+                  image: DecorationImage(
+                    fit: BoxFit.cover,
+                    image: NetworkImage(
+                      widget.productImage,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 10),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  SizedBox(
+                    height: 5,
+                  ),
+                  Text(
+                    widget.productName,
+                    overflow: TextOverflow.ellipsis,
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 8,
+                  ),
+                  Text(
+                    widget.productModel,
+                    overflow: TextOverflow.ellipsis,
+                    style: TextStyle(
+                      color: Colors.black,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 8,
+                  ),
+                  Row(
+                    children: [
+                      Text(
+                        "\$ ${widget.productPrice}",
+                        overflow: TextOverflow.ellipsis,
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                      SizedBox(
+                        width: 15,
+                      ),
+                      Text(
+                        "\$ ${widget.productOldPrice}",
+                        overflow: TextOverflow.ellipsis,
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            decoration: TextDecoration.lineThrough),
+                      ),
+                    ],
+                  ),
+                  SizedBox(
+                    height: 8,
+                  ),
+                  Row(
+                    children: [
+                      Icon(
+                        Icons.star,
+                        color: Colors.amber,
+                      ),
+                      Icon(
+                        Icons.star,
+                        color: Colors.amber,
+                      ),
+                      Icon(
+                        Icons.star,
+                        color: Colors.amber,
+                      ),
+                      Icon(
+                        Icons.star_half,
+                        color: Colors.amber,
+                      ),
+                    ],
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
