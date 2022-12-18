629a865d497ffde1a5e06be76d2e18fbbc59ff0e
sundipthapa <lakhanmgr123@gmail.com>
2022-12-18 12:26:48 +0545

    :hammer: all product list added

--- lib/Screens/homeScreen/homepage_data.dart
+++ lib/Screens/homeScreen/homepage_data.dart
@@ -1,1 +1,109 @@
+import '../../Model/SingleProductModel.dart';
 
+List<SingleProductModel> sigleProductData = [
+  SingleProductModel(
+      productImage:
+          'https://res.cloudinary.com/dyfwfkx4t/image/upload/v1671271287/H7417d9abc34d463182854179354f7d80m.jpg_300x300_ivhgtu.jpg',
+      productModel: 'Training',
+      productName: 'Training weave backpack',
+      productOldPrice: 200,
+      productPrice: 150,
+      productFourImage:
+          'https://assets.reebok.com/images/w_600,f_auto,q_auto/1fc379faa6b0473e8516ac8a010031b5_9366/Training_Weave_Backpack_Black_GT7686_04_standard.jpg',
+      productSecondImage:
+          'https://assets.reebok.com/images/w_600,f_auto,q_auto/44df69de09e24e5db910abc100f3f30a_9366/Training_Weave_Backpack_Yellow_GH0037_04_standard.jpg',
+      productThirdImage:
+          'https://produkty.lionsport.cz/produkty_images/600/gh0099/gh0099.jpg'),
+  SingleProductModel(
+      productImage:
+          'https://res.cloudinary.com/dyfwfkx4t/image/upload/v1671269395/Ha07ee1360df445c49ecc6ebf1891e462U.jpg_720x720q50_mppixr.jpg',
+      productModel: 'Training',
+      productName: 'Training weave backpack',
+      productOldPrice: 200,
+      productPrice: 150,
+      productFourImage:
+          'https://assets.reebok.com/images/w_600,f_auto,q_auto/1fc379faa6b0473e8516ac8a010031b5_9366/Training_Weave_Backpack_Black_GT7686_04_standard.jpg',
+      productSecondImage:
+          'https://assets.reebok.com/images/w_600,f_auto,q_auto/44df69de09e24e5db910abc100f3f30a_9366/Training_Weave_Backpack_Yellow_GH0037_04_standard.jpg',
+      productThirdImage:
+          'https://produkty.lionsport.cz/produkty_images/600/gh0099/gh0099.jpg'),
+  SingleProductModel(
+      productImage:
+          'https://res.cloudinary.com/dyfwfkx4t/image/upload/v1671272235/A9f02a6fb3d284324ba6d55e6f6250424w.png_300x300_ctaxel.png',
+      productModel: 'T-shirt',
+      productName: 'Allen Iverson I3 3I Logo T-Shirt',
+      productOldPrice: 40,
+      productPrice: 30,
+      productFourImage:
+          'https://assets.reebok.com/images/w_600,f_auto,q_auto/3705e28fc76c462abbecacf10033b39d_9366/Allen_Iverson_I3_3I_Logo_T-Shirt_Multi_EY0885_03_standard_hover.jpg',
+      productSecondImage:
+          'https://assets.reebok.com/images/w_600,f_auto,q_auto/4a84ea20a9ec4b3b8864acf1002f6623_9366/Allen_Iverson_I3_3I_Logo_T-Shirt_Multi_EY0885_05_detail.jpg',
+      productThirdImage:
+          'https://assets.reebok.com/images/w_600,f_auto,q_auto/27ea5cd9696f45b8968aacf1002bc8ce_9366/Allen_Iverson_I3_Dimensional_Logo_T-Shirt_Multi_EY0880_01_standard.jpg'),
+  SingleProductModel(
+      productImage:
+          'https://assets.reebok.com/images/w_600,f_auto,q_auto/eb31ff6778fa42e89e57ac480039b8fd_9366/Classics_Archive_Backpack_Small_Black_GN7640.jpg',
+      productModel: 'Classic',
+      productName: 'Classics archive backpack',
+      productOldPrice: 70,
+      productPrice: 54,
+      productFourImage:
+          'https://assets.reebok.com/images/w_600,f_auto,q_auto/4c3801f601b647279640aaca0105c7f1_9366/Classics_Archive_Backpack_Black_FL7045_04_standard.jpg',
+      productSecondImage:
+          'https://assets.reebok.com/images/w_600,f_auto,q_auto/f509c6c223e947798d50ab3601497153_9366/Classics_Archive_Backpack_Black_GD1040_01_standard.jpg',
+      productThirdImage:
+          'https://assets.reebok.com/images/w_600,f_auto,q_auto/53b4eac625434cffbfc1ab1400e1f2bc_9366/Classics_Archive_Backpack_White_FL7044_04_standard.jpg'),
+  SingleProductModel(
+      productImage:
+          'https://res.cloudinary.com/dyfwfkx4t/image/upload/v1671273371/S2f232e46d59f4549b0a4707b479d39c2i.jpg_300x300_qjnuk2.jpg',
+      productModel: 'training',
+      productName: 'Active Enhanced Convertible Grip Bag',
+      productOldPrice: 85,
+      productPrice: 70,
+      productFourImage:
+          'https://assets.reebok.com/images/w_600,f_auto,q_auto/9d2ce122e3bd4b838e9dabc100ea51b9_9366/Active_Enhanced_Convertible_Grip_Bag_Orange_GH0112_04_standard.jpg',
+      productSecondImage:
+          'https://assets.reebok.com/images/w_600,f_auto,q_auto/dd709bb49870430bb2dfab0500bf9e76_9366/Active_Enhanced_Grip_Bag_Black_FQ5368_04_standard.jpg',
+      productThirdImage:
+          'https://assets.reebok.com/images/w_600,f_auto,q_auto:sensitive,fl_lossy/d280d49fa0e8400da15ca9880020b2e0_9366/Active_Enhanced_Grip_Bag_Large_Black_DU2968_04_standard.jpg'),
+  SingleProductModel(
+      productImage:
+          'https://assets.adidas.com/images/w_600,f_auto,q_auto/5adc6dabf52a4b23a97baa060176638a_9366/Rivalry_RM_Low_Shoes_Beige_EE4989_01_standard.jpg',
+      productModel: 'Sneakers',
+      productName: 'Rivalry RM Low',
+      productOldPrice: 158,
+      productPrice: 130,
+      productFourImage:
+          'https://assets.adidas.com/images/w_600,f_auto,q_auto/590d3dfc6f7c43f2a492aa070012601b_9366/Rivalry_RM_Low_Shoes_Beige_EE4989.jpg',
+      productSecondImage:
+          'https://assets.adidas.com/images/w_600,f_auto,q_auto/64515af34c5844289b5daa060178bd6f_9366/Rivalry_RM_Low_Shoes_Beige_EE4989_05_standard.jpg',
+      productThirdImage:
+          'https://assets.adidas.com/images/w_600,f_auto,q_auto/991b7aee890647449e80aa06017ee41c_9366/Rivalry_RM_Low_Shoes_Beige_EE4989_03_standard.jpg'),
+  SingleProductModel(
+      productImage:
+          'https://res.cloudinary.com/dyfwfkx4t/image/upload/v1671273217/H045da1cb89554bc29a160cf07151ea70F.jpg_300x300_jqhtgr.jpg',
+      productModel: 'Sneakers',
+      productName: 'Rivalry RM Low',
+      productOldPrice: 158,
+      productPrice: 130,
+      productFourImage:
+          'https://assets.adidas.com/images/w_600,f_auto,q_auto/d142bbed034f488e850caa4e00885595_9366/Rivalry_RM_Low_Shoes_Black_EE4987.jpg',
+      productSecondImage:
+          'https://assets.adidas.com/images/w_600,f_auto,q_auto/94da80a6059249629624aa4e0088612b_9366/Rivalry_RM_Low_Shoes_Black_EE4987_05_standard.jpg',
+      productThirdImage:
+          'https://assets.adidas.com/images/w_600,f_auto,q_auto/d142bbed034f488e850caa4e00885595_9366/Rivalry_RM_Low_Shoes_Black_EE4987.jpg'),
+  SingleProductModel(
+      productImage:
+          'https://res.cloudinary.com/dyfwfkx4t/image/upload/v1671270315/Hda1aa34eb6894f3788884ca65eda2a2fZ.jpg_720x720q50_gm40e7.jpg',
+      productModel: 'Classic',
+      productName: 'Classics archive backpack',
+      productOldPrice: 70,
+      productPrice: 54,
+      productFourImage:
+          'https://assets.reebok.com/images/w_600,f_auto,q_auto/4c3801f601b647279640aaca0105c7f1_9366/Classics_Archive_Backpack_Black_FL7045_04_standard.jpg',
+      productSecondImage:
+          'https://assets.reebok.com/images/w_600,f_auto,q_auto/f509c6c223e947798d50ab3601497153_9366/Classics_Archive_Backpack_Black_GD1040_01_standard.jpg',
+      productThirdImage:
+          'https://assets.reebok.com/images/w_600,f_auto,q_auto/53b4eac625434cffbfc1ab1400e1f2bc_9366/Classics_Archive_Backpack_White_FL7044_04_standard.jpg'),
+];
+////////////////////////////// Hoodies.............
