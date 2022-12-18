401368c8a29990e8bd90510fc165a6ec4796277b
sundipthapa <lakhanmgr123@gmail.com>
2022-12-18 12:23:38 +0545

    :hammer: showallwidgets file

--- lib/widgets/show_allwidgets.dart
+++ lib/widgets/show_allwidgets.dart
@@ -1,1 +1,25 @@
+import 'package:flutter/material.dart';
 
+class ShowAllWidget extends StatelessWidget {
+  final String leftText;
+  ShowAllWidget({required this.leftText});
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: 25),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Text(
+            leftText,
+            style: TextStyle(
+              fontSize: 17,
+              color: Colors.black54,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
