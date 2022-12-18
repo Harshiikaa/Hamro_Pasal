885dc65dffdd91d883632d64c242ccdc617f978c
sunsilthapa <sunsil1281@gmail.com>
2022-12-18 12:39:21 +0545

    :lipstick: home screen file updated

--- lib/Screens/homeScreen/homeScreen.dart
+++ lib/Screens/homeScreen/homeScreen.dart
@@ -1,1 +1,40 @@
+import 'package:flutter/material.dart';
 
+import '../../Model/SingleProductModel.dart';
+import '../../widgets/Single_product_widget.dart';
+import '../../widgets/all_category.dart';
+import '../../widgets/homeappbar.dart';
+import '../../widgets/show_allwidgets.dart';
+import 'homepage_data.dart';
+
+void main() {
+  runApp(const MaterialApp(
+    home: HomeScreen(),
+    debugShowCheckedModeBanner: false,
+  ));
+}
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen>
+    with SingleTickerProviderStateMixin {
+  static SingleProductModel? signleProductModel;
+  late TabController tabController;
+
+  @override
+  void initState() {
+    tabController = TabController(length: 6, vsync: this);
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    tabController.dispose();
+    super.dispose();
+  }
+}
