4fe3bd665ec1c7c43560fa98a093ea56eb1698a6
admiral41 <kafle.susan671@outlook.com>
2022-12-27 16:18:21 +0545

    :hammer: linked all products so that search function works

--- lib/Screens/Search/search.dart
+++ lib/Screens/Search/search.dart
@@ -1,15 +1,43 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import '../../Model/SingleProductModel.dart';
+import '../Detailscreen/detail_screens.dart';
+import '../homeScreen/homepage_data.dart';
 
 class SearchPage extends StatefulWidget {
-  const SearchPage({Key? key}) : super(key: key);
-
+  SearchPage({Key? key}) : super(key: key);
   @override
   State<SearchPage> createState() => _SearchPageState();
 }
 
 class _SearchPageState extends State<SearchPage> {
+  List<SingleProductModel> _filteredProducts = [];
+// Search query
+  String _searchQuery = '';
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    for (SingleProductModel product in _filteredProducts) {
+      String productName = product.productName;
+      String productImage = product.productImage;
+      double productPrice = product.productPrice;
+
+    }
+    _filteredProducts = sigleProductData +
+        colothsData +
+        shoesData +
+        accessoriesData +
+        tshirtsData +
+        pantsData;
+  }
+
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      
+    );
   }
 }
