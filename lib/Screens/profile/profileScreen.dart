590d4b99426c316178b3710617e03488aba4eb67
admiral41 <kafle.susan671@outlook.com>
2023-01-04 19:15:08 +0545

    :hammer: added personal data and change your email

--- lib/Screens/profile/profileScreen.dart
+++ lib/Screens/profile/profileScreen.dart
@@ -30,10 +30,63 @@ class _ProfileScreenState extends State<ProfileScreen> {
         color: Color(0xFFD6D6D6),
         child: ListView(
           children: [
+            colorTiles(),
+
           ],
         ),
       ),
       bottomNavigationBar: logoutButton(),
+    );
+  }
+
+  Widget colorTiles() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+      child: Container(
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(20),
+        ),
+        child: Column(
+          children: [
+            ListTile(
+              leading: Container(
+                child: Icon(Icons.person_outline, color: Colors.deepPurple),
+                height: 45,
+                width: 45,
+                decoration: BoxDecoration(
+                  color: Colors.deepPurple.withOpacity(0.09),
+                  borderRadius: BorderRadius.circular(18),
+                ),
+              ),
+              title: Text("Personal Data",
+                  style: TextStyle(fontWeight: FontWeight.w700)),
+              trailing: Icon(Icons.arrow_forward_ios,
+                  color: Colors.deepOrange, size: 20),
+              onTap: () {},
+            ),
+            divider(),
+            ListTile(
+              leading: Container(
+                child: Icon(Icons.email_outlined, color: Colors.blue),
+                height: 45,
+                width: 45,
+                decoration: BoxDecoration(
+                  color: Colors.blue.withOpacity(0.09),
+                  borderRadius: BorderRadius.circular(18),
+                ),
+              ),
+              title: Text("Change your email",
+                  style: TextStyle(fontWeight: FontWeight.w700)),
+              trailing: Icon(Icons.arrow_forward_ios,
+                  color: Colors.deepOrange, size: 20),
+              onTap: () {},
+            ),
+            divider(),
+
+          ],
+        ),
+      ),
     );
   }
 
