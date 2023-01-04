b7879432badcc427a8d9bc32035a1412dd4b05a3
sunsilthapa <sunsil1281@gmail.com>
2023-01-04 20:09:51 +0545

    :lipstick: feature added in faqs

--- lib/Screens/profile/faqs.dart
+++ lib/Screens/profile/faqs.dart
@@ -1,54 +1,92 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
-void main() {
-  runApp(const MyApp());
+import '../../Model/Item_model.dart';
+
+class ExpansionPanelDemo extends StatefulWidget {
+  @override
+  _ExpansionPanelDemoState createState() => _ExpansionPanelDemoState();
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
+class _ExpansionPanelDemoState extends State<ExpansionPanelDemo> {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Hamro-Pasal',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text('FAQs'),
       ),
-      home: FrequentlyAskedQuestions(),
+      body: Container(
+        padding: EdgeInsets.all(10),
+        child: ListView.builder(
+          itemCount: itemData.length,
+          itemBuilder: (BuildContext context, int index) {
+            return ExpansionPanelList(
+              animationDuration: Duration(milliseconds: 1000),
+              dividerColor: Colors.red,
+              elevation: 1,
+              children: [
+                ExpansionPanel(
+                  body: Container(
+                    padding: EdgeInsets.all(10),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Text(
+                          itemData[index].discription,
+                          style: TextStyle(
+                              color: Colors.grey[700],
+                              fontSize: 15,
+                              letterSpacing: 0.3,
+                              height: 1.3),
+                        ),
+                      ],
+                    ),
+                  ),
+                  headerBuilder: (BuildContext context, bool isExpanded) {
+                    return Container(
+                      padding: EdgeInsets.all(10),
+                      child: Text(
+                        itemData[index].headerItem,
+                        style: TextStyle(
+                          color: itemData[index].colorsItem,
+                          fontSize: 18,
+                        ),
+                      ),
+                    );
+                  },
+                  isExpanded: itemData[index].expanded,
+                )
+              ],
+              expansionCallback: (int item, bool status) {
+                setState(() {
+                  itemData[index].expanded = !itemData[index].expanded;
+                });
+              },
+            );
+          },
+        ),
+      ),
     );
   }
 }
 
-class FrequentlyAskedQuestions extends StatefulWidget {
-  const FrequentlyAskedQuestions({Key? key}) : super(key: key);
-
-  @override
-  State<FrequentlyAskedQuestions> createState() =>
-      _FrequentlyAskedQuestionsState();
+// import 'package:flutter/cupertino.dart';
+// import 'package:flutter/material.dart';
+//
+void main() {
+  runApp(MyApp());
 }
 
-class _FrequentlyAskedQuestionsState extends State<FrequentlyAskedQuestions> {
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text("FAQs")),
-      body: const ExpansionTile(
-        title: Text(
-          "What is Hamropasal?",
-          style: TextStyle(fontSize: 12, fontWeight: FontWeight.w600),
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          primarySwatch: Colors.teal,
         ),
-        children: [
-          ListTile(
-            title: Text(
-              "this is ecommerce website which perform seel and buy of things online.",
-              style: TextStyle(fontSize: 12, fontWeight: FontWeight.w100),
-            ),
-          )
-        ],
-      ),
-    );
+        home: ExpansionPanelDemo());
   }
 }
