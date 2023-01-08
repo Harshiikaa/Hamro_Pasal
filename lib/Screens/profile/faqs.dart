b3595b09bf1a297133ae99d36e9ba866bfb04831
sunsilthapa <sunsil1281@gmail.com>
2023-01-08 20:57:00 +0545

    file updated

--- lib/Screens/profile/faqs.dart
+++ lib/Screens/profile/faqs.dart
@@ -136,22 +136,3 @@ class _FAQsScreenState extends State<FAQsScreen> {
     ),
   ];
 }
-
-// import 'package:flutter/cupertino.dart';
-// import 'package:flutter/material.dart';
-//
-void main() {
-  runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        theme: ThemeData(
-          primarySwatch: Colors.orange,
-        ),
-        home: FAQsScreen());
-  }
-}
