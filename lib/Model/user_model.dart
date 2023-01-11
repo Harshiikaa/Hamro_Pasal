7f1cfa8654cdae4756a6d714e39301d3261f91e0
sunsilthapa <sunsil1281@gmail.com>
2023-01-11 20:40:04 +0545

    user model updated

--- lib/Model/user_model.dart
+++ lib/Model/user_model.dart
@@ -1,11 +1,11 @@
 import 'dart:convert';
 
 import 'package:cloud_firestore/cloud_firestore.dart';
-//
-// UserModel? productModelFromJson(String str) =>
-//     UserModel.fromJson(json.decode(str));
-//
-// String productModelToJson(UserModel? data) => json.encode(data!.toJson());
+
+UserModel? productModelFromJson(String str) =>
+    UserModel.fromJson(json.decode(str));
+
+String productModelToJson(UserModel? data) => json.encode(data!.toJson());
 
 class UserModel {
   String? id;
@@ -18,6 +18,21 @@ class UserModel {
       required this.fullname,
       required this.email,
       required this.password});
+
+  factory UserModel.fromFirebaseSnapshot(
+          DocumentSnapshot<Map<String, dynamic>> json) =>
+      UserModel(
+        id: json.id,
+        fullname: json["fullName"],
+        email: json["email"],
+        password: json["password"],
+      );
+
+  factory UserModel.fromJson(Map<String, dynamic> json) => UserModel(
+      id: json["id"],
+      fullname: json["fullname"],
+      email: json["email"],
+      password: json["password"]);
 
   Map<String, dynamic> toJson() => {
         "id": id,
