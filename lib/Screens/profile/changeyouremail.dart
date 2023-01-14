5c1d40b434f5788a18fda74558654acc911fdd07
admiral41 <kafle.susan671@outlook.com>
2023-01-14 19:29:44 +0545

    :hammer: added change meial , verify email, confirm email section

--- lib/Screens/profile/changeyouremail.dart
+++ lib/Screens/profile/changeyouremail.dart
@@ -38,4 +38,160 @@ class _ChangeEmailState extends State<ChangeEmail> {
       bottomNavigationBar: logoutButton(),
     );
   }
+  Widget colorTiles() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 50),
+      child: Container(
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(20),
+        ),
+        child: Column(
+          children: [
+            Container(
+              padding: EdgeInsets.only(top: 20),
+              child: Column(
+                children: <Widget>[
+                  Text("Previous Email:", style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                    color:Colors.black,
+                  ),),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 30),
+                    child: TextFormField(
+                        controller: emailController,
+                        validator: (value) {
+                          if (value != null || value!.isEmpty) {
+                            final bool isValid =
+                            EmailValidator.validate(emailController.text.trim());
+                            if (!isValid) {
+                              return "Invalid email";
+                            }
+                          }
+                        },
+                        keyboardType: TextInputType.emailAddress,
+                        cursorColor: Colors.deepOrange,
+                        cursorHeight: 25,
+                        decoration: InputDecoration(
+                          filled: true,
+                          fillColor: Colors.grey[350],
+                          prefixIcon: Icon(
+                            Icons.email_rounded,
+                            color: Colors.deepOrange,
+                            size: 25,
+                          ),
+                          hintText: "Enter your previous email...",
+                          border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(30),
+                              borderSide: BorderSide.none),
+                          focusedBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(30),
+                              borderSide: BorderSide(color: Colors.black38)),
+                        )
+                    ),
+                  ),
+                ],
+              ),
+            ),divider(),
+            Container(
+              padding: EdgeInsets.only(top: 10),
+              child: Column(
+                children:<Widget>[
+                  Text("New Email:", style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                    color:Colors.black,
+                  ),),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 30),
+                    child: TextFormField(
+                        controller: emailController,
+                        validator: (value) {
+                          if (value != null || value!.isEmpty) {
+                            final bool isValid =
+                            EmailValidator.validate(emailController.text.trim());
+                            if (!isValid) {
+                              return "Invalid email";
+                            }
+                          }
+                        },
+                        keyboardType: TextInputType.emailAddress,
+                        cursorColor: Colors.deepOrange,
+                        cursorHeight: 25,
+                        decoration: InputDecoration(
+                          filled: true,
+                          fillColor: Colors.grey[350],
+                          prefixIcon: Icon(
+                            Icons.email_rounded,
+                            color: Colors.deepOrange,
+                            size: 25,
+                          ),
+                          hintText: "Enter your new email",
+                          border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(30),
+                              borderSide: BorderSide.none),
+                          focusedBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(30),
+                              borderSide: BorderSide(color: Colors.black38)),
+                        )
+                    ),
+                  ),
+                ],
+              ),
+            ),divider(),
+
+            Container(
+              padding: EdgeInsets.only(top: 10),
+              child: Column(
+                children: <Widget>[
+                  Text("Confirm-Email:", style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                    color:Colors.black,
+                  ),),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 30),
+                    child: TextFormField(
+                        controller: emailController,
+                        validator: (value) {
+                          if (value != null || value!.isEmpty) {
+                            final bool isValid =
+                            EmailValidator.validate(emailController.text.trim());
+                            if (!isValid) {
+                              return "Invalid email";
+                            }
+                          }
+                        },
+                        keyboardType: TextInputType.emailAddress,
+                        cursorColor: Colors.deepOrange,
+                        cursorHeight: 25,
+                        decoration: InputDecoration(
+                          filled: true,
+                          fillColor: Colors.grey[350],
+                          prefixIcon: Icon(
+                            Icons.email_rounded,
+                            color: Colors.deepOrange,
+                            size: 25,
+                          ),
+                          hintText: "Verify your email",
+                          border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(30),
+                              borderSide: BorderSide.none),
+                          focusedBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(30),
+                              borderSide: BorderSide(color: Colors.black38)),
+                        )
+                    ),
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
+
+      ),
+    );
+  }
+
 }
