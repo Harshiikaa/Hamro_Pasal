1986cb236c4a41ae038aee2d70ea101a8c5125fe
admiral41 <kafle.susan671@outlook.com>
2023-01-14 19:28:55 +0545

    :hammer: added app bar

--- lib/Screens/profile/changeyouremail.dart
+++ lib/Screens/profile/changeyouremail.dart
@@ -1,4 +1,6 @@
+import 'package:email_validator/email_validator.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class ChangeEmail extends StatefulWidget {
   const ChangeEmail({Key? key}) : super(key: key);
@@ -8,8 +10,32 @@ class ChangeEmail extends StatefulWidget {
 }
 
 class _ChangeEmailState extends State<ChangeEmail> {
+  Widget divider() {
+    return Padding(
+      padding: const EdgeInsets.all(0.5),
+      child: Divider(
+        thickness: 1.5,
+      ),
+    );
+  }
+  TextEditingController emailController = TextEditingController();
   @override
   Widget build(BuildContext context) {
-    return Placeholder();
+    return Scaffold(
+      appBar:  AppBar(
+        centerTitle: true,
+        title: Text('Change your Email'),
+        backgroundColor: Colors.deepOrange,
+      ),
+      body: Container(
+        color: Color(0xFFD6D6D6),
+        child: ListView(
+          children: [
+            colorTiles(),
+          ],
+        ),
+      ),
+      bottomNavigationBar: logoutButton(),
+    );
   }
 }
