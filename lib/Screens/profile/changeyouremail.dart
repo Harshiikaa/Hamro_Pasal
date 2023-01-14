08e5692e4410d65c597208fbb4c7a8717a1375d3
admiral41 <kafle.susan671@outlook.com>
2023-01-14 19:27:17 +0545

    :tada: changeyouremail init

---
+++ lib/Screens/profile/changeyouremail.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+
+class ChangeEmail extends StatefulWidget {
+  const ChangeEmail({Key? key}) : super(key: key);
+
+  @override
+  State<ChangeEmail> createState() => _ChangeEmailState();
+}
+
+class _ChangeEmailState extends State<ChangeEmail> {
+  @override
+  Widget build(BuildContext context) {
+    return Placeholder();
+  }
+}
