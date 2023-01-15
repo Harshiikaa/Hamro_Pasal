2b2f2a83166a8380df800de924479aa18cab1b0d
admiral41 <kafle.susan671@outlook.com>
2023-01-15 18:34:59 +0545

    :sparkles: payment init

---
+++ lib/Screens/shoppingCart/payment.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+class ePayment extends StatefulWidget {
+  const ePayment({Key? key}) : super(key: key);
+
+  @override
+  State<ePayment> createState() => _ePaymentState();
+}
+
+class _ePaymentState extends State<ePayment> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
